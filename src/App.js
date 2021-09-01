5a4cf2e444b47f866e230e4bfd8a7a3449aca41f
MouryaManu <67451638+MouryaManu@users.noreply.github.com>
2021-09-01 21:37:29 +0530

    Added colorHelpers.js to generate colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import {generatePalette} from './colorHelpers';
 
 function App() {
+  console.log(generatePalette(seedColors[3]));
   return (
     <div className="App">
       
