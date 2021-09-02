f24858eb2b21693393959fb1cbc1f0d420600811
MouryaManu <67451638+MouryaManu@users.noreply.github.com>
2021-09-02 14:47:10 +0530

    Added React Routers Basic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import React, { Component } from 'react'
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelpers';
 
-function App() {
-  
-  return (
-    <div>
-      
-     <Palette palette= {generatePalette(seedColors[3])} />
+class App extends Component {
+  render() {
+   return (
+     <div>
+       <Switch>
+       <Route exact path="/" render={() => <h1>Palette list comes here</h1>} />
+       <Route exact path="/palette/:id" render={()=> <h1>Individual Palette is here!</h1>}  />
+       </Switch>
+     {/* <Palette palette= {generatePalette(seedColors[3])} /> */}
 
     </div>
-  );
+   );
+ }
 }
 
 export default App;
