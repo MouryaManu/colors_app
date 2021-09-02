e6ba1f559050e3abf2c2c488d25e445ee9954dbc
MouryaManu <67451638+MouryaManu@users.noreply.github.com>
2021-09-02 15:12:09 +0530

    Added Palette List and Links for individual Palettes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelpers';
 
@@ -17,7 +18,7 @@ class App extends Component {
        <Switch>
        <Route exact 
        path="/" 
-       render={() => <h1>Palette list comes here</h1>} 
+       render={() => <PaletteList palettes={seedColors} /> } 
        />
 
        <Route exact 
