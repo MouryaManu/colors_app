702ea6b896ecb114da211c334c4022960adc846a
MouryaManu <67451638+MouryaManu@users.noreply.github.com>
2021-09-05 11:44:31 +0530

    Refactored Navbar CSS and deleted some CSS files and wrote in JSS(Self-Contained Styles JSS)

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
+import styles from './styles/NavBarStyles'
+import {withStyles} from "@material-ui/styles";
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -7,7 +9,6 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import "./Navbar.css";
 
 
 class Navbar extends Component {
@@ -20,7 +21,7 @@ class Navbar extends Component {
         this.handleFormatChange=this.handleFormatChange.bind(this);
         this.closeSnackbar= this.closeSnackbar.bind(this);
     }
-
+    
     handleFormatChange(e) {
         this.setState({
             format: e.target.value,
@@ -28,35 +29,35 @@ class Navbar extends Component {
         })
         this.props.handleChange(e.target.value);
     }
-
+    
     closeSnackbar() {
         this.setState ({
             open: false
         })
     }
-
-
+    
+    
     render() {
-        const {level, levelChange, showingAllColors} = this.props;
+        const {level, levelChange, showingAllColors,classes} = this.props;
         return (
-            <header className="Navbar">
-                <div className="logo">
-                    <Link to="/">reactcolorpicker</Link>
-                </div>
-               {showingAllColors && (
-                    <div className="slider-container">
-                    <span>Level: {level} </span>
-                <div className='slider'>
+            <header className={classes.Navbar}>
+            <div className={classes.logo} >
+            <Link to="/">reactcolorpicker</Link>
+            </div>
+            {showingAllColors && (
+                <div className="slider-container">
+                <span>Level: {level} </span>
+                <div className={classes.slider} >
                 <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={levelChange} />
                 </div>
                 </div>
-                 )}
-                <div className='select-container'>
-                  <Select value={this.state.format} onChange={this.handleFormatChange}>
-                      <MenuItem value='hex'>HEX- #ffffff</MenuItem>
-                      <MenuItem value='rgb'>RGB- rgb(255,255,255)</MenuItem>
-                      <MenuItem value='rgba'>RGBA- rgba(255,255,255,0.3)</MenuItem>
-                  </Select>
+                )}
+                <div className={classes.selectContainer} >
+                <Select value={this.state.format} onChange={this.handleFormatChange}>
+                <MenuItem value='hex'>HEX- #ffffff</MenuItem>
+                <MenuItem value='rgb'>RGB- rgb(255,255,255)</MenuItem>
+                <MenuItem value='rgba'>RGBA- rgba(255,255,255,0.3)</MenuItem>
+                </Select>
                 </div>
                 <Snackbar 
                 anchorOrigin={{vertical: "bottom", horizontal: "left"}} 
@@ -71,14 +72,14 @@ class Navbar extends Component {
                     key='close'
                     aria-label='close' 
                     >
-                        <CloseIcon />
+                    <CloseIcon />
                     </IconButton>
                 ]}
                 />
-            </header>
-
-        )
-    }
-}
-
-export default Navbar;
+                </header>
+                
+                )
+            }
+        }
+        
+        export default withStyles(styles)(Navbar);
