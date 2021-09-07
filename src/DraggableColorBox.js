e2314e804fd4e66e2d5f47461fa16f43bb85352a
MouryaManu <67451638+MouryaManu@users.noreply.github.com>
2021-09-07 16:15:45 +0530

    Added Delete SVG for draggable Color Box

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import DeleteIcon from '@material-ui/icons/Delete';
 import { withStyles } from '@material-ui/styles'
+
 
 
 const styles= {
@@ -10,18 +12,44 @@ const styles= {
         display: "inline-block",
         position: "relative",
         cursor: "pointer",
-        marginBottom: "-3.5px"
+        marginBottom: "-3.5px",
+        "&:hover svg" : {
+            color: 'white',
+            transform: "scale(1.5)"
+        }
+    },
+    boxContent : {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        position: "absolute",
+        bottom: "0px",
+        left: "0px",
+        padding: "10px",
+        textTransform: "uppercase",
+        letterSpacing: "1px",
+        fontSize: "12px"
+    },
+    
+    deleteIcon: {
+        color: 'rgba(0,0,0,0.5)',
+        transition: "all 0.3s ease-in-out",
+        
     }
 
 }
 
 function DraggableColorBox(props) {
-   
+   const {classes} = props;
     return (
-        <div 
-        className={props.classes.root}
-        style={{ backgroundColor: props.color}}>
-            {props.name}
+        <div className={classes.root} style={{ backgroundColor: props.color}}>
+         <div className={classes.boxContent}>
+            <span>
+                {props.name}
+            </span>
+            <DeleteIcon className={classes.deleteIcon} />
+         </div>
         </div>
     )
 }
